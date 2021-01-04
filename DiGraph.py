fd4c831a6cd8c015984c42168a798d64f91ddc7d
Guppi4 <kirill.perevalov.1996@gmail.com>
2021-01-04 17:44:38 +0200

    all_out_edges_of_node

--- DiGraph.py
+++ DiGraph.py
@@ -19,15 +19,25 @@ class DiGraph(GraphInterface):
     return self.edgeSize
 
  def  get_all_v(self):
-     return self.graph.values()
+     return self.graph
 
  def all_in_edges_of_node(self,id):
     t={}
-    for key in self.graph.values():
+    for key in self.graph.keys():
          if(self.graph[key].getEdge(id)!=None):
-             t0=self.graph[key].getEdge(id)
-             t[key]=self.graph[key]
+             t[key] = self.graph[key].getEdge(id).w
+
     return t
+ def all_out_edges_of_node(self, id1):
+     t={}
+     for key in self.graph[id1].neighbors:
+            t[key]=self.graph[id1].neighbors[key].w
+     return t
+
+
+
+
+
 
 
 
@@ -36,8 +46,16 @@ class DiGraph(GraphInterface):
 
 if __name__ == "__main__":
     t=DiGraph()
-    t2=NodeData()
+    t2=NodeData(1,7)
     t.graph[1]=t2
-    t3=t.graph[1]
-    print(t3.Info)
+    t3 = NodeData(2,99)
+    t2.createEdge(1,2,9)
+    t.graph[2]=t3
+    t4 = NodeData(3, 4)
+    t4.createEdge(2, 1, 8)
+    t.graph[3] = t4
+    print(t.all_in_edges_of_node(1))
+    #print(t.get_all_v())
+    print(t.all_out_edges_of_node(1))
+
 
